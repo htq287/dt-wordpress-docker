8d3982d6da3d94eefc8ae8bb3b51e13812fb932c
Hung Q. Truong <htq287@gmail.com>
2020-09-21 23:49:25 +0700

    * add script to import db

--- import_db.sh
+++ import_db.sh
@@ -1,1 +1,19 @@
 #!/bin/bash
+
+# Start mysql service
+docker-compose --log-level ERROR up -d example_com_db
+
+# Load database name + root password
+source .env
+
+# Wait
+while ! (docker-compose exec wizylance_blog_db /usr/bin/mysqladmin -u $MYSQL_USER --password=${MYSQL_ROOT_PASSWORD} ping --silent)
+do
+	sleep 3
+	echo "Wait for DB to initialize"
+done
+
+# Restore
+echo "Starting restore now."
+cat $DB_BACKUP | docker-compose exec -T example_com_db \
+	/usr/bin/mysql -u $MYSQL_USER --password=${MYSQL_ROOT_PASSWORD} ${DB_NAME}
